e6f1175acc3f6d6b013cc42d77d0883a679dc587
yumoqing <yumoqing@gmail.com>
2019-05-14 10:50:40 +0800

    delete build dist

--- build/lib/sqlor/aiomysqlor.py
+++
@@ -1,7 +0,0 @@
-from .mysqlor import MySqlor
-
-class AioMysqlor(MySqlor):
-	@classmethod
-	def isMe(self,name):
-		return name=='aiomysql'
-
--- build/lib/sqlor/aiopostgresqlor.py
+++
@@ -1,8 +0,0 @@
-
-from .postgresqlor import PostgreSQLor
-class AioPostgresqlor(PostgreSQLor):
-	@classmethod
-	def isMe(self,name):
-		return name=='aiopg'
-
-
